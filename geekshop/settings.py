51acc058e7866e6ef469692d90d302deaa66d902
zakirovrj <rjzakirov@gmail.com>
2020-12-06 16:10:18 +0100

    Lesson 4 + HM 4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp',
+    'authapp'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
+from mainapp.models import ProductCategory, Product
+
 
 import json
 import datetime
 import os
-
-# Create your views here.
-from mainapp.models import Product
 
 
 def main(request):
@@ -25,7 +24,8 @@ def main(request):
     # }
 
     title = 'главная'
-    products = Product.objects.all()[:4]
+    # products = Product.objects.all()[:4]
+    products = Product.objects.all()
     content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', content)
 
@@ -33,45 +33,46 @@ def main(request):
 def products(request, pk=None):
     print(pk)
     title = 'продукты'
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
-
-    same_products = [
-        {
-            'name': 'Отличный стул',
-            'desc': 'Не оторваться',
-            'image_src': 'product-11.jpg',
-            'alt': 'продукт 11'
-        },
-        {
-            'name': 'Стул повышенного качества',
-            'desc': 'комфортно',
-            'image_src': 'product-21.jpg',
-            'alt': 'продукт 21'
-        },
-        {
-            'name': 'Стул премиального качества',
-            'desc': 'росто попробуйте ',
-            'image_src': 'product-31.jpg',
-            'alt': 'продукт 31'
-        },
-    ]
+    # links_menu = [
+    #     {'href': 'products_all', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    # same_products = [
+    #     {
+    #         'name': 'Отличный стул',
+    #         'desc': 'Не оторваться',
+    #         'image_src': 'product-11.jpg',
+    #         'alt': 'продукт 11'
+    #     },
+    #     {
+    #         'name': 'Стул повышенного качества',
+    #         'desc': 'комфортно',
+    #         'image_src': 'product-21.jpg',
+    #         'alt': 'продукт 21'
+    #     },
+    #     {
+    #         'name': 'Стул премиального качества',
+    #         'desc': 'росто попробуйте ',
+    #         'image_src': 'product-31.jpg',
+    #         'alt': 'продукт 31'
+    #     },
+    # ]
 
     content = {
         'title': title,
-        # 'links_menu': links_menu,
+        'links_menu': links_menu,
         'same_products': same_products
     }
     return render(request, 'mainapp/products.html', content)
 
 
 def contacts(request):
-    title = ''
+    title = 'о нас'
     visit_date = datetime.datetime.now()
     locations = []
     file_path = os.path.join(settings.BASE_DIR, 'contacts.json')
