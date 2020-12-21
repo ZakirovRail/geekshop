cc46c61c9f976f8386f00dc5595841f9aa178b01
zakirovrj <rjzakirov@gmail.com>
2020-12-21 21:43:42 +0100

    Lesson 1  - Adding sending of an email and confirmation of an activation

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,3 +1,6 @@
+import hashlib
+import random
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
@@ -34,6 +37,16 @@ class ShopUserRegisterForm(UserCreationForm):
         if data < 18:
             raise forms.ValidationError('Вы слишком молоды')
         return data
+
+    def save(self):
+        user = super().save()
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf-8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf-8')).hexdigest()
+        user.save()
+
+        return user
+
 
 
 class ShopUserEditForm(UserChangeForm):
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -136,3 +136,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'authapp.ShopUser'
 
 LOGIN_URL = '/auth/login/'
+
+
+DOMAIN_NAME = 'http://localhost:8000'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+# EMAIL_HOST_USER = 'django@geekbrains.local'
+# EMAIL_HOST_PASSWORD = 'geekshop'
+EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
