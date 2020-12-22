18bbb7d70f74fdb15a84ded5040219df1251a32b
zakirovrj <rjzakirov@gmail.com>
2020-12-22 23:24:57 +0100

    Lesson 1 - changing adminapp/views to CVB

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 import adminapp.views as adminapp
 
@@ -19,10 +19,13 @@ urlpatterns = [
     # path('categories/delete/<pk>', adminapp.category_delete, name='category_delete'),
     path('categories/delete/<int:pk>', adminapp.ProductCategoryDeleteView.as_view(), name='category_delete'),
 
-    path('products/create/category/<pk>', adminapp.product_create, name='product_create'),
-    path('products/read/category/<pk>', adminapp.products, name='products'),
+    # path(r'products/create/category/<pk>', adminapp.product_create, name='product_create'),
+    re_path(r'^products/create/category/(?P<pk>\d+)/$', adminapp.ProductCreateView.as_view(), name='product_create'),
+    # path('products/read/category/<pk>', adminapp.products, name='products'),
+    re_path(r'^products/read/category/(?P<pk>\d+)/$', adminapp.ProductListView.as_view(), name='products'),
     # path('products/read/<pk>', adminapp.product_read, name='product_read'),
     path('products/read/<pk>', adminapp.ProductDetailView.as_view(), name='product_read'),
     path('products/update/<pk>', adminapp.product_update, name='product_update'),
-    path('products/delete/<pk>', adminapp.product_delete, name='product_delete'),
+    # path('products/delete/<pk>', adminapp.product_delete, name='product_delete'),
+    re_path(r'^products/delete/(?P<pk>\d+)/$', adminapp.ProductDeleteView.as_view(), name='product_delete'),
 ]
