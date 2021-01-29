56d6b943f8e8c6ff24ce4bac9c288e827e4ba536
zakirovrj <rjzakirov@gmail.com>
2021-01-29 11:00:13 +0100

    Refatoring for adminapp

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,26 +7,18 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('users/create/', adminapp.user_create, name='user_create'),
-    # path('users/read/', adminapp.users, name='users'),
     path('users/read/', adminapp.UsersListView.as_view(), name='users'),
     path('users/update/<pk>', adminapp.user_update, name='user_update'),
     path('users/delete/<pk>', adminapp.user_delete, name='user_delete'),
 
     path('categories/create/', adminapp.ProductCategoryCreateView.as_view(), name='category_create'),
     path('categories/read/', adminapp.categories, name='categories'),
-    # path('categories/update/<pk>', adminapp.category_update, name='category_update'),
     path('categories/update/<int:pk>', adminapp.ProductCategoryUpdateView.as_view(), name='category_update'),
-    # path('categories/delete/<pk>', adminapp.category_delete, name='category_delete'),
     path('categories/delete/<int:pk>', adminapp.ProductCategoryDeleteView.as_view(), name='category_delete'),
 
-    # path(r'products/create/category/<pk>', adminapp.product_create, name='product_create'),
     re_path(r'^products/create/category/(?P<pk>\d+)/$', adminapp.ProductCreateView.as_view(), name='product_create'),
-    # path('products/read/category/<pk>', adminapp.products, name='products'),
     re_path(r'^products/read/category/(?P<pk>\d+)/$', adminapp.ProductListView.as_view(), name='products'),
-    # path('products/read/<pk>', adminapp.product_read, name='product_read'),
     path('products/read/<pk>', adminapp.ProductDetailView.as_view(), name='product_read'),
     path('products/update/<pk>', adminapp.product_update, name='product_update'),
-    # re_path(r'^products/update/(?P<pk>\d+)/$', adminapp.ProductUpdateView.as_view(), name='product_update'),
-    # path('products/delete/<pk>', adminapp.product_delete, name='product_delete'),
     re_path(r'^products/delete/(?P<pk>\d+)/$', adminapp.ProductDeleteView.as_view(), name='product_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -5,47 +5,26 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.db import connection
 
 from adminapp.forms import ShopUserAdminEditForm, ProductEditForm, ProductCategoryEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
-
 from mainapp.models import ProductCategory, Product
 
-from django.dispatch import receiver
-from django.db.models.signals import pre_save
-from django.db import connection
-
-
-# users
-
-# если убрать декоратор, то будет возможность не залогиненому пользователю перейти на страницу админки с пользователями
-# http://127.0.0.1:8000/admin/users/read/
-# прописать в требованиях
-# @user_passes_test(lambda u: u.is_superuser)
-# def users(request):
-#     title = 'Админка/пользователи'
-#     user_list = ShopUser.objects.all().order_by('-is_active')
-#     content = {
-#         'title': title,
-#         'objects': user_list
-#     }
-#     return render(request, 'adminapp/users.html', content)
 
 class UsersListView(ListView):
     model = ShopUser
     template_name = 'adminapp/users.html'
-    paginate_by = 3  # простой способ отображения кол-ва пользователей
-    # в требовниях указать "правильное" отображение количества зарегистрированных пользователей
+    paginate_by = 3
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
-    #  если убрать этот метод, то название вкладки страницы будет корявым - сделать как дефект
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Админка/ППользователи'  # опечатка в названии вкладки
+        context['title'] = 'Админка/Пользователи'
         return context
 
 
@@ -63,9 +42,6 @@ def user_create(request):
         'update_form': user_form
     }
     return render(request, 'adminapp/user_update.html', content)
-
-
-
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -101,25 +77,6 @@ def user_delete(request, pk):
     return render(request, 'adminapp/user_delete.html', content)
 
 
-#  categories
-# @user_passes_test(lambda u: u.is_superuser)
-# def category_create(request):
-#     title = 'категории/создание'
-#     if request.method == 'POST':
-#         category_form = ProductCategoryEditForm(request.POST, request.FILES)
-#         if category_form.is_valid():
-#             category_form.save()
-#             return HttpResponseRedirect(reverse('admin:categories'))
-#     else:
-#         category_form = ProductCategoryEditForm()
-#     content = {
-#         'title': title,
-#         'update_form': category_form
-#     }
-#
-#     return render(request, 'adminapp/category_update.html', content)
-
-
 class ProductCategoryCreateView(CreateView):
     model = ProductCategory
     template_name = 'adminapp/category_update.html'
@@ -131,7 +88,6 @@ class ProductCategoryCreateView(CreateView):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
-    #  если убрать этот метод, то название вкладки страницы будет корявым - сделать как дефект
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'категории/создание'
@@ -147,26 +103,6 @@ def categories(request):
     return render(request, 'adminapp/categories.html', content)
 
 
-# @user_passes_test(lambda u: u.is_superuser)
-# def category_update(request, pk):
-#     title = 'категория/Обновить'
-#     edit_category = get_object_or_404(ProductCategory, pk=pk)
-#
-#     if request.method == 'POST':
-#         edit_form = ProductCategoryEditForm(request.POST, request.FILES, instance=edit_category)
-#         if edit_form.is_valid():
-#             edit_form.save()
-#             return HttpResponseRedirect(reverse('adminapp:category_update', args=[edit_category.pk]))
-#     else:
-#         edit_form = ProductCategoryEditForm(instance=edit_category)
-#
-#     content = {
-#         'title': title,
-#         'update_form': edit_form,
-#     }
-#     return render(request, 'adminapp/category_update.html', content)
-
-
 def db_profile_by_type(prefix, type, queries):
     update_queries = list(filter(lambda x: type in x['sql'], queries))
     print(f'db_profile {type} for {prefix}:')
@@ -178,7 +114,6 @@ class ProductCategoryUpdateView(UpdateView):
     template_name = 'adminapp/category_update.html'
     success_url = reverse_lazy('adminapp:categories')
     form_class = ProductCategoryEditForm
-
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, *args, **kwargs):
@@ -193,32 +128,9 @@ class ProductCategoryUpdateView(UpdateView):
         if 'discount' in form.cleaned_data:
             discount = form.cleaned_data['discount']
             if discount:
-                # print(f'Применяется скидка {discount}% к товарам категории {self.object_name}')
-                # на текущий момент, если указывать размер скидки в % на странице редактирования катеогрий
-                # http://127.0.0.1:8000/admin/categories/update/1 ,
-                # то применяется скидка ко всем товарам так, что получается новая цена, т.е. цена режется
-                # и размер скидки не сохраняется на странице редактирования категории товаров
-                self.object.product_set.update(price=F('price') * (1-discount/100))  # !!! ERROR description below
+                self.object.product_set.update(price=F('price') * (1-discount/100))
                 db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
         return super().form_valid(form)
-
-
-# @user_passes_test(l ambda u: u.is_superuser)
-# def category_delete(request, pk):
-#     title = 'категории/удаление'
-#
-#     category = get_object_or_404(ProductCategory, pk=pk)
-#
-#     if request.method == 'POST':
-#         category.is_active = False
-#         category.save()
-#         return HttpResponseRedirect(reverse('admin:categories'))
-#
-#     content = {
-#         'title':title,
-#         'category_to_delete': category
-#     }
-#     return render(request, 'adminapp/category_delete.html', content)
 
 
 class ProductCategoryDeleteView(DeleteView):
@@ -235,22 +147,6 @@ class ProductCategoryDeleteView(DeleteView):
         self.object.is_active = False
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
-
-
-# products
-# @user_passes_test(lambda u: u.is_superuser)
-# def products(request, pk):
-#     title = 'Админка/Продукты'
-#     category_item = get_object_or_404(ProductCategory, pk=pk)
-#     products_list = Product.objects.filter(category=category_item).order_by('name')  # можно убрать сортировку по имени
-#                                                                                      # и сделать дефект
-#
-#     content = {
-#         'title': title,
-#         'objects': products_list,
-#         'category': category_item,
-#     }
-#     return render(request, 'adminapp/products.html', content)
 
 
 class ProductListView(ListView):
@@ -305,37 +201,6 @@ class ProductCreateView(CreateView):
         success_url = reverse('adminapp:products', args=[category_pk])
         return success_url
 
-# @user_passes_test(lambda u: u.is_superuser)
-# def product_create(request, pk):
-#     title = 'Админка/Создать'
-#     category_item = get_object_or_404(ProductCategory, pk=pk)
-#
-#     if request.method == 'POST':
-#         update_form = ProductEditForm(request.POST, request.FILES)
-#         if update_form.is_valid():
-#             update_form.save()
-#             return HttpResponseRedirect(reverse('admin:products', args=[pk]))
-#     else:
-#         update_form = ProductEditForm(initial={'category': category_item})
-#         # update_form = ProductEditForm() # если оставить таким образом, то при создании нового товара категории
-#         #  категория товара не будет автоматически проставляться - можно как баш оформить
-#     content = {
-#         'title': title,
-#         'update_form': update_form,
-#         'category': category_item
-#     }
-#     return render(request, 'adminapp/product_update.html', content)
-
-
-# @user_passes_test(lambda u: u.is_superuser)
-# def product_read(request, pk):
-#     title = 'Админка/Read'
-#     product_item = get_object_or_404(Product, pk = pk)
-#     content = {
-#         'title': title,
-#         'object': product_item,
-#     }
-#     return render(request, 'adminapp/product_read.html', content)
 
 class ProductDetailView(DetailView):
     model = Product
@@ -365,40 +230,6 @@ def product_update(request, pk):
         'category': product_item.category
     }
     return render(request, 'adminapp/product_update.html', content)
-
-
-# Раскоментировать когда в случае необходимсти переделать всю логику для обновления информации
-# по товару - url, template, view
-# class ProductUpdateView(UpdateView):
-#     model = Product
-#     form_class = ProductEditForm
-#
-#     def get_success_url(self):
-#         pk = self.kwargs['pk']
-#         product_item = Product.objects.get(pk=pk)
-#         return reverse('adminapp:products', args=[product_item.category__pk])
-#
-#     @method_decorator(user_passes_test(lambda u: u.is_superuser))
-#     def dispatch(self, *args, **kwargs):
-#         return super().dispatch(*args, **kwargs)
-
-
-# @user_passes_test(lambda u: u.is_superuser)
-# def product_delete(request, pk):
-#     title = 'продукт/удаление'
-#
-#     product = get_object_or_404(Product, pk=pk)
-#
-#     if request.method == 'POST':
-#         product.is_active = False
-#         product.save()
-#         return HttpResponseRedirect(reverse('admin:products', args=[product.category_id]))
-#
-#     content = {
-#         'title': title,
-#         'product_to_delete': product
-#     }
-#     return render(request, 'adminapp/product_delete.html', content)
 
 
 class ProductDeleteView(DeleteView):
